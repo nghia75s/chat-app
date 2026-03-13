7ee5be69794301c2cbebf50e527b571fedfa6b3a
nghia75s <nghia85b@gmail.com>
2026-03-14 02:11:16 +0700

    add

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,4 +1,11 @@
 import bcrypt from 'bcrypt';
+import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
+import Session from '../models/Session.js';
+
+const ACCESS_TOKEN_TLL = '30m';
+const REFRESH_TOKEN_TLL = 14*24*60*60*1000;
 
 export const signUp = async (req, res) => {
     try {
@@ -23,9 +30,52 @@ export const signUp = async (req, res) => {
             displayName: `${firstName} ${lastName}`,
         });
 
-        return res.status(204);
+        return res.sendStatus(204);
     } catch (error) {
         console.error(error);
         return res.status(500).json({ message: "Internal server error" });
     }
 };
+
+export const signIn = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        if (!username || !password) {
+            return res.status(400).json({ message: "Username and password are required" });
+        }
+
+        const user = await User.findOne({username});
+
+        if (!user) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.hashedPassword);
+
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+
+        const accessToken = jwt.sign({ userId: user._id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: ACCESS_TOKEN_TLL });
+        const refreshToken = crypto.randomBytes(64).toString('hex');
+
+        await Session.create({
+            userId: user._id,
+            refreshToken,
+            expiresAt: new Date(Date.now() + REFRESH_TOKEN_TLL),
+        });
+
+        res.cookie('refreshToken', refreshToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none',
+            maxAge: REFRESH_TOKEN_TLL,
+        });
+        
+        return res.status(200).json({ message: "Sign in successful", accessToken });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}; 
--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 
-app.use('api/auth', authRoute);
+app.use('/api/auth', authRoute);
 
 connectDB().then(() => {
     app.listen(PORT, () => {
